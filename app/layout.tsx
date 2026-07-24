64e66e6c9a047f0e9bd9de452404611388e53154
AnterMena <antermena.studio@gmail.com>
2026-07-24 11:04:04 +0100

    post: 24-07-2026

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export const metadata: Metadata = {
   alternates: {
     canonical: "https://365loan.ca",
   },
-  openGraph: {
+  openGraph: { images: [{ url: "/opengraph-image.png", width: 1200, height: 630 }],
     type: "website",
     locale: "en_CA",
     url: "https://365loan.ca",
